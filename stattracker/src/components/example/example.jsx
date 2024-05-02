f549b8e49dcdb01bf30e5d2cf9fe4b4680116382
pablosb01 <pablosb01@icloud.com>
2024-05-02 15:24:44 +0200

    development basic push

---
+++ stattracker/src/components/example/example.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Style from './example.module.css'
+
+function Example() {
+  return (
+    <div className={Style.prueba}>example</div>
+  )
+}
+
+export default Example
