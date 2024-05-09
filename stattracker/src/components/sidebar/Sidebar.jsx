451e596f0bc214e8cbbb8d92e79275d23a60c98e
pablosb01 <pablosb01@icloud.com>
2024-05-09 15:45:45 +0200

    sidebar push with svg components

--- stattracker/src/components/sidebar/Sidebar.jsx
+++ stattracker/src/components/sidebar/Sidebar.jsx
@@ -1,26 +1,37 @@
-import React, { useState } from "react"
-import './Sidebar.css'
-import sidebarIcon from './logo-small.png'
-import sidebarIcon2 from './logo-big.png'
+import React, { useState } from "react";
+import "./Sidebar.css";
+import sidebarIcon from "./logo-small.png";
+import sidebarIcon2 from "./logo-big.png";
+import { SvgRow } from "../SvgRow/SvgRow";
 
 export function Sidebar() {
-    
-    const [isOpen, setIsOpen] = useState(false)
-    const [buttonImage, setButtonImage] = useState(sidebarIcon)
+  const [isOpen, setIsOpen] = useState(false);
+  const [buttonImage, setButtonImage] = useState(sidebarIcon);
 
-    const toggleSidebar = () => {
-        setIsOpen(!isOpen);
-        setButtonImage(isOpen ? sidebarIcon : sidebarIcon2)
-    }
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+    setButtonImage(isOpen ? sidebarIcon : sidebarIcon2);
+  };
 
-    console.log(isOpen)
+  console.log(isOpen);
 
-
-    return(
-        <div className='global-container'>
-            <div className={`sidebar ${isOpen ? 'open' : ''}`}>
-                <img src={buttonImage} onClick={toggleSidebar} className={'logo-small'}/>
-            </div>
+  return (
+    <div className="global-container">
+      <div className={`sidebar ${isOpen ? "open" : ""}`}>
+        <div className={"logo-container"}>
+          <img
+            src={buttonImage}
+            onClick={toggleSidebar}
+            className={"logo-small"}
+          />
         </div>
-    )
+        <div className={"svg-container"}>
+          <SvgRow name={'GitHub'}/>
+          <SvgRow name={'Instagram'}/>
+          <SvgRow name={'VALORANT'}/>
+          <SvgRow name={'Honkai'}/>
+        </div>
+      </div>
+    </div>
+  );
 }
