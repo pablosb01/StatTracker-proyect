6f502ae028c8f9683cad27060a4d4c1ae138f074
pablosb01 <pablosb01@icloud.com>
2024-05-27 19:05:28 +0200

    home progreso

---
+++ stattracker/src/components/downloadbutton/DownloadButton.jsx
@@ -0,0 +1,10 @@
+export function Download() {
+
+    return(
+        <>
+            <button className='bg-red-600 w-1/2 h-14 flex rounded justify-center items-center hover:bg-red-300'>
+                <p className='font-sans bold'>Download</p>
+            </button>
+        </>
+    )
+}
--- stattracker/src/views/valorant-home/valorant-home.jsx
+++ stattracker/src/views/valorant-home/valorant-home.jsx
@@ -3,6 +3,7 @@ import UserFormValorant from '../../components/UserFormValorant/UserFormValorant
 import LeaderBoard from '../../components/leaderboard/Leaderboard'
 import matchInfo from '/src/objects/matchinfo.js'
 import ValoLogo from '/src/assets/valorant-logo.png'
+import { Download } from '../../components/downloadbutton/DownloadButton'
 
 export function ValoHome() {
     console.log(matchInfo)
@@ -10,14 +11,15 @@ export function ValoHome() {
 
     return(
         <>
-            <div className='flex items-center justify-center'>
-                <div className=''>
-                    <img className='w-[450px] flex justify-center'src={ValoLogo}/>
+            <div className='flex items-center gap-3 justify-center py-10 md:flex-col lg:flex-row'>
+                <div className='relative w-1/2 flex flex-col gap-16 items-center'>
+                    <img className='w-1/2 m-auto'src={ValoLogo}/>
                     <UserFormValorant/>
                     <Agents/>
                 </div>
-                <div className='flex items-center justify-center'>
+                <div className='flex items-center flex-col gap-16 justify-center'>
                     <LeaderBoard/>
+                    <Download/>
                 </div>
             </div>
         </>
