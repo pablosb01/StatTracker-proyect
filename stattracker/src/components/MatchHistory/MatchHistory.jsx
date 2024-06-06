af3d1e8d574dd4b70b490f55b8c06eccef0eebbd
DnlSglsPrz <daniel.segales.perez@hotmail.com>
2024-06-06 02:55:02 +0200

    Implemented some logic in MatchStats

--- stattracker/src/components/MatchHistory/MatchHistory.jsx
+++ stattracker/src/components/MatchHistory/MatchHistory.jsx
@@ -9,9 +9,15 @@ export function MatchHistory() {
             <p className="text-left items-center p-1.5 pl-3 text-3xl text-white font-sans">Match History</p>
         </div>
         <div>
-            <MatchComplete/>
-            <MatchComplete/>
-            <MatchComplete/>
+            <MatchComplete puuid="Hng3HRgjI7CMrBDFH4jd6TS08gALhYD_muPm9JKN2bcwT_s-wOBumpfcrWeSXv7OVydqBXSRunlUsA"/>
+            <MatchComplete puuid="HgBQup9MZhYkMyqcw8-HzEqxdL-u4JXfCjAsrTVMSYBTq2djY791__r-XOXx1vkgBXlNmgUvDLxp9Q"/>
+            <MatchComplete puuid="Y2X9ZAnysEx5cxszvriWCL62y4Q5upG5-CFHafgqUgOgrAk1HHficYZRoOxcdN9bri4rMlcv8798bQ"/>
+            <MatchComplete puuid="inxcpz8Bw4qzirO6sd0OPv4q3SnRzLV0ql4Q2XUq65aDRqfrjGwY3Sj54rr0W9qpvmTtINbI0VNITw"/>
+            <MatchComplete puuid="qGVUmtCNCmGxJmD4F6iqT53Sca5IrHFfnvMDqOFtWvTiuuaHTszfpug7XaLUIbjEcl0amcrJtTeuuA"/>
+            <MatchComplete puuid="gaHjFv0Kkg8f-saVWPIqUjE6Dcdnc3ohM6Ffvjq4Ud9NP7ynfzvl1tL0EwRKqGYkOYPAyjf_V-BLNA"/>
+            <MatchComplete puuid="vnE1JAJyVlf08fJIwuo4zPMjIZZuvlChvEYsDPmMZbdT1Dbg0rmtDLUr1Z22TTfP4pHGEyT-FmbuPA"/>
+            <MatchComplete puuid="FjXIt87aLFSWcVkZJhxiTyAgf90zeiz2yjiPQtxbmng8oYlFeqwS9ziS7-Er8NClXt2ephk_gS754g"/>            
+
         </div>
     </div>
   )
--- stattracker/src/components/MatchHistory/MatchStats.jsx
+++ stattracker/src/components/MatchHistory/MatchStats.jsx
@@ -1,31 +1,76 @@
-export function MatchStats() {
-  return (
-    <div className="w-3/4">
-        <div className="flex justify-between">
-            <p className="">+150 RR</p>
-            <div className="flex gap-1">
-                <p>10th</p>
-                <p>Competitive</p>
+import gameData from "../../objects/matchinfo";
+
+export function MatchStats({playerid}) {
+
+    const puuid = playerid;
+    
+    function getPlayerStatsByPuuid(puuid) {
+        const player = gameData.players.find(p => p.puuid === puuid);
+      
+        if (!player) {
+          throw Error = "User ID not found"
+        }
+      
+        return {
+          kills: player.stats.kills,
+          deaths: player.stats.deaths,
+          assists: player.stats.assists,
+          score: player.stats.score,
+        };
+    }
+
+    const playerStats = getPlayerStatsByPuuid(puuid);
+
+    function calculateTotalDamage(puuid) {
+        let totalDamage = 0;
+      
+        gameData.roundResults.forEach(round => {
+          round.playerStats.forEach(playerStat => {
+            if (playerStat.puuid === puuid) {
+              playerStat.damage.forEach(damageEntry => {
+                totalDamage += damageEntry.damage;
+              });
+            }
+          });
+        });
+      
+        return totalDamage;
+    }
+    
+    const totalDamage = calculateTotalDamage(puuid)
+    const numberOfRounds = gameData.roundResults.length;
+    const mpr = (playerStats.kills / numberOfRounds).toFixed(2);
+    const kda = ((playerStats.kills + playerStats.assists)/playerStats.deaths).toFixed(2);
+    const dpr = (totalDamage/numberOfRounds).toFixed(2);
+    const averageScore = (playerStats.score/ numberOfRounds).toFixed(0);
+  
+    return (
+        <div className="w-3/4">
+            <div className="flex justify-between">
+                <p className="">+150 RR</p> {/* STILL HARDCODED */}
+                <div className="flex gap-1">
+                    <p>10th</p> {/* STILL HARDCODED */}
+                    <p>Competitive</p> {/* STILL HARDCODED */}
+                </div>
+            </div>
+            <div className="flex">
+                <div className="w-1/5">
+                    <p className="text-orange-400">{kda} KDA</p>
+                    <p className="text-sm text-gray-300">{playerStats.kills}/{playerStats.deaths}/{playerStats.assists}</p>
+                </div>
+                <div className="w-1/5">
+                    <p>{mpr} MPR</p>
+                    <p className="text-sm text-gray-300">{dpr} DPR</p>
+                </div>
+                <div className="w-2/5">
+                    <p>21,7% Cabeza</p> {/* STILL HARDCODED */}
+                    <p className="text-sm text-gray-300">{averageScore} Puntuación media</p>
+                </div>
+                <div className="w-1/5">
+                    <div></div>
+                    <p className="text-sm text-gray-300">anteayer</p> {/* STILL HARDCODED */}
+                </div>
             </div>
         </div>
-        <div className="flex">
-            <div className="w-1/5">
-                <p className="text-orange-400">0,60 KDA</p>
-                <p className="text-sm text-gray-300">6/19/5</p>
-            </div>
-            <div className="w-1/5">
-                <p>0,30 MPR</p>
-                <p className="text-sm text-gray-300">82,50 DPR</p>
-            </div>
-            <div className="w-2/5">
-                <p>21,7% Cabeza</p>
-                <p className="text-sm text-gray-300">106 Puntuación media</p>
-            </div>
-            <div className="w-1/5">
-                <div></div>
-                <p className="text-sm text-gray-300">anteayer</p>
-            </div>
-        </div>
-    </div>
-  )
+    )
 }
