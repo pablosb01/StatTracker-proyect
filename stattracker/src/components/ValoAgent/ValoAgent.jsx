a0f6d1b2fb25000af18feeb214e602e05c4b71d5
pablosb01 <pablosb01@icloud.com>
2024-06-15 16:23:12 +0200

    agents funcionando

--- stattracker/src/components/ValoAgent/ValoAgent.jsx
+++ stattracker/src/components/ValoAgent/ValoAgent.jsx
@@ -1,6 +1,7 @@
 import { useOutletContext } from "react-router-dom"
 import { useEffect, useState } from 'react'
 import { useParams } from "react-router-dom"
+import { Ability } from "../Ability/Ability"
 
 export function ValoAgent (){
     const [backgroundGrandient, setBackgroundGradient] = useState()
@@ -20,10 +21,10 @@ export function ValoAgent (){
 
     return(
         <div className='flex flex-row w-full'>
-            <div className='relative w-fit h-screen px-2'>
+            <div className='relative w-fit h-screen px-2 my-auto'>
                 <img src={currentAgent.background} alt={currentAgent.name} className={`absolute top-0 left-0 h-full object-cover z-0 ${backgroundGrandient}`}/>
                 <div className='relative z-10 flex items-center justify-center w-full h-full'>
-                    <img src={currentAgent.bustPortrait} className={`max-w-xl w-full h-full filter drop-shadow-[0_0_10px_#ffffffff] object-cover`}/>
+                    <img src={currentAgent.bustPortrait} className={`max-w-xl w-full h-full object-cover`} style={{filter: `drop-shadow(0 0 10px #${currentAgent.backgroundGradientColors[0]})`}}/>
                 </div>
             </div>
             <div className='mx-auto p-5 w-3/5'>
@@ -37,6 +38,25 @@ export function ValoAgent (){
                         </span>
                     </span>
                 </span>
+                <h3 className='text-white text-xl py-5'>{currentAgent.description}</h3>
+                <div className='border border-white-600 border-2 p-10 [clip-path:_polygon(20%_0,_100%_0,_100%_80%,_80%_100%,0_100%,_0_20%)]'>
+                    <h2 className='text-white text-4xl mx-auto font-fat w-fit'>ABILITIES:</h2>
+                    {/* <div className='flex flex-col gap-6 border border-red-600 border-2 p-8 [clip-path:_polygon(20%_0,_100%_0,_100%_80%,_80%_100%,0_100%,_0_20%)]'>
+                        <div className='flex items-center justify-left flex-row gap-4'>
+                            <img className='h-8 w-8'src={currentAgent.abilities[0].displayIcon}/>
+                            <h3 className='text-gray-400 text-xl'>{currentAgent.abilities[0].displayName}</h3>
+                        </div>
+                        <div>
+                            <p className='text-white text-lg'>{currentAgent.abilities[0].description}</p>
+                        </div>
+                    </div> */}
+
+                    {/* <Ability ab={currentAgent.abilities[0]}/> */}
+
+                    {currentAgent.abilities.map((ab, index) => 
+                        (<Ability ab={ab}/>)
+                    )}
+                </div>
             </div>
         </div>
     )
--- stattracker/src/components/imageNavbar/ImageNavbar.jsx
+++ stattracker/src/components/imageNavbar/ImageNavbar.jsx
@@ -4,13 +4,13 @@ export function ImageNavbar(object) {
 
     
     return (
-        <nav className="w-screen flex sticky justify-between bg-stone-800 px-5 gap-1 top-0 pt-3">
+        <nav className="w-screen flex sticky justify-between bg-stone-800 px-2 gap-1 top-0 pt-3">
             {Object.values(object).map(arr => 
                 arr.map((agent, index) => {
                     if (agent.displayName && agent.displayIcon) {
                         return (
                             <NavLink className={({ isActive }) => 
-                                `relative flex hover:scale-[140%] bg-gradient-to-b to-gray-600 from-red-800 transition z-10 hover:z-50 duration-200 scale ease-in-out [clip-path:_polygon(20%_0,_100%_0,_100%_80%,_80%_100%,0_100%,_0_20%)] ${isActive ? 'bg-stone-800 bg-gradient-to-b to-green-600 from-red-800border-gray-400 text-red-600 ' : 'hover:bg-stone-800 bg-gradient-to-b to-gray-600 from-red-800 text-white'}`
+                                `relative flex hover:scale-[140%] bg-gradient-to-b to-gray-600 from-red-800 transition z-10 hover:z-50 duration-200 scale ease-in-out [clip-path:_polygon(20%_0,_100%_0,_100%_80%,_80%_100%,0_100%,_0_20%)] ${isActive ? 'bg-stone-800 bg-gradient-to-b to-gray-600 from-green-800 border border-white-600 border-2 text-red-600 ' : 'hover:bg-stone-800 bg-gradient-to-b to-gray-600 from-red-800 text-white'}`
                             }
                             key={index} to={agent.displayName.toLowerCase().replace("/","")}>
                                 <img className='' src={agent.displayIcon} alt={agent.displayName}/>
