bf437d7059e3ecd84b25a195196d20a242340b88
pablosb01 <pablosb01@icloud.com>
2024-06-17 16:42:17 +0200

    riot text

--- stattracker/src/views/riot/RiotView.jsx
+++ stattracker/src/views/riot/RiotView.jsx
@@ -1,11 +1,11 @@
-// src/RiotText.js
+
 import React, { useEffect, useState } from 'react';
 
 const RiotView = () => {
   const [content, setContent] = useState('');
 
   useEffect(() => {
-    fetch('/riot.txt')
+    fetch('./riot.txt')
       .then((response) => response.text())
       .then((text) => {
         setContent(text);
@@ -18,7 +18,7 @@ const RiotView = () => {
   return (
     <div>
       <h1>Riot Text Content</h1>
-      <pre>{content}</pre>
+      <pre>99b746d7-b8bf-4cd4-91f8-201cdec7a066</pre>
     </div>
   );
 };
