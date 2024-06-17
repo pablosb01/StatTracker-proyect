bd17e24956a5c0fb29fa24af4b3a70f89416cf70
pablosb01 <pablosb01@icloud.com>
2024-06-17 12:32:02 +0200

    try 2

--- stattracker/src/views/honkai-user.jsx/honkai-user.jsx
+++ stattracker/src/views/honkai-user.jsx/honkai-user.jsx
@@ -9,7 +9,7 @@ export function HonkaiUser() {
     const [error, setError] = useState();
 
     useEffect(() => {
-        fetch(`https://api.mihomo.me/sr_info_parsed/${user}?lang=en`, /* { mode: 'no-cors' } */)
+        fetch(`https://api.mihomo.me/sr_info_parsed/${user}?lang=en`, { mode: 'no-cors' })
             .then(res => res.json())
             .then(data => {
                 setApiInfo(data);
