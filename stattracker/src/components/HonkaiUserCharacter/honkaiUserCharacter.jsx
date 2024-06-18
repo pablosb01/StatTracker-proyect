b4e2470a0703b92dcc2139389393e4cf5162aaf5
DnlSglsPrz <daniel.segales.perez@hotmail.com>
2024-06-18 18:32:10 +0200

    working on Honk-user

--- stattracker/src/components/HonkaiUserCharacter/honkaiUserCharacter.jsx
+++ stattracker/src/components/HonkaiUserCharacter/honkaiUserCharacter.jsx
@@ -1,30 +1,39 @@
 import HonkaiUserCharArtifacts from "./HonkaiUserCharArtifacts";
 import HonkaiUserLightCone from "./HonkaiUserCharLightCone";
-
 import HonkaiUserCharStats from "./HonkaiUserCharStats";
-import HonkaiUserCharOrnaments from "./HonkaiUserCharOrnaments"
+import HonkaiUserCharOrnaments from "./HonkaiUserCharOrnaments";
 import BoxHuc from "./BoxHuc";
 import HonkaiUserCharSkillsboxed from "./HonkaiUserCharSkillsboxed";
 import HonkaiUserCharSplash from "./HonkaiUserCharSplash";
 
-export function HonkaiUserCharacter({obj}) {
+export function HonkaiUserCharacter({ obj }) {
   return (
     <div className=" w-full lg:max-w-[1300px] grid grid-cols-3 lg:grid-cols-7 gap-2 rounded-lg ">
-      <HonkaiUserCharSplash obj={obj[0]}/>
-      <BoxHuc title={`${obj[0].name.toUpperCase()}`} stl="col-span-1 row-span-4 order-2 lg:col-span-2 lg:row-span-6 lg:order-2">
-        <HonkaiUserCharStats obj={obj[0]}/>
+      <HonkaiUserCharSplash obj={obj[0]} />
+      <BoxHuc
+        title={`${obj[0].name.toUpperCase()}`}
+        stl="col-span-1 row-span-4 order-2 lg:col-span-2 lg:row-span-6 lg:order-2">
+        <HonkaiUserCharStats obj={obj[0]} />
       </BoxHuc>
-      <BoxHuc title="LIGHT CONE" stl="col-span-1 row-span-2 order-3 lg:col-span-2 lg:row-span-2 lg:order-5">
-        <HonkaiUserLightCone obj={obj[0].light_cone}/>
+      <BoxHuc
+        title="LIGHT CONE"
+        stl="col-span-1 row-span-2 order-3 lg:col-span-2 lg:row-span-2 lg:order-5">
+        <HonkaiUserLightCone obj={obj[0].light_cone} />
       </BoxHuc>
-      <BoxHuc title="SKILLS" stl="col-span-1 row-span-2 order-4 lg:col-span-2 lg:row-span-3 lg:order-6">
-        <HonkaiUserCharSkillsboxed/>
+      <BoxHuc
+        title="SKILLS"
+        stl="col-span-1 row-span-2 order-4 lg:col-span-2 lg:row-span-3 lg:order-6">
+        <HonkaiUserCharSkillsboxed />
       </BoxHuc>
-      <BoxHuc title="ARTIFACTS" stl="col-span-2 row-span-3 order-5 lg:col-span-2 lg:row-span-6 lg:order-4">
-        <HonkaiUserCharArtifacts/>
+      <BoxHuc
+        title="ARTIFACTS"
+        stl="col-span-2 row-span-3 order-5 lg:col-span-2 lg:row-span-6 lg:order-4">
+        <HonkaiUserCharArtifacts />
       </BoxHuc>
-      <BoxHuc title="ORNAMENTS" stl="col-span-1 row-span-3 order-6 lg:col-span-2 lg:row-span-5 lg:order-3">
-        <HonkaiUserCharOrnaments/>
+      <BoxHuc
+        title="ORNAMENTS"
+        stl="col-span-1 row-span-3 order-6 lg:col-span-2 lg:row-span-5 lg:order-3">
+        <HonkaiUserCharOrnaments />
       </BoxHuc>
     </div>
   );
