5b0233b3508235f38738a19f026e02dc2196880d
pablosb01 <pablosb01@icloud.com>
2024-06-19 12:38:58 +0200

    agents view almost finished

--- stattracker/src/components/ValoAgent/ValoAgent.jsx
+++ stattracker/src/components/ValoAgent/ValoAgent.jsx
@@ -2,6 +2,9 @@ import { useOutletContext } from "react-router-dom"
 import { useEffect, useState } from 'react'
 import { useParams } from "react-router-dom"
 import { Ability } from "../Ability/Ability"
+import { AgentRole } from "../AgentRole/AgentRole"
+import { AbilitySM } from "../Ability/AbilitySM"
+
 
 export function ValoAgent (){
     const [backgroundGrandient, setBackgroundGradient] = useState()
@@ -9,6 +12,7 @@ export function ValoAgent (){
     const currentAgent = useOutletContext()
 
     return(
+        <>
         <div className='flex flex-row w-full hidden xl:flex'>
             <div className='relative w-fit h-screen px-2 my-auto'>
                 <img src={currentAgent.background} alt={currentAgent.name} className={`absolute top-0 left-0 h-full object-cover z-0`}/>
@@ -19,14 +23,9 @@ export function ValoAgent (){
             <div className='mx-auto p-5 w-3/5'>
                 <span className='flex flex-row gap-5 justify-between items-center'>
                     <h1 className='text-white text-6xl font-fat border-b-2 border-red-600'>{currentAgent.displayName.toUpperCase()}</h1>
-                    <span className='flex flex-col justify-between items-center gap-2 border border-red-600 border-2 p-4 [clip-path:_polygon(20%_0,_100%_0,_100%_80%,_80%_100%,0_100%,_0_20%)]'>
-                        <h2 className='text-white text-xl'>Role:</h2>
-                        <span className='flex flex-row gap gap-4'>
-                            <img className='h-10'src={currentAgent.role.displayIcon}/>
-                            <h1 className='text-gray-400 text-3xl'>{currentAgent.role.displayName}</h1>
-                        </span>
-                    </span>
+                    <AgentRole currentAgent={currentAgent}/>
                 </span>
+                <AgentRole currentAgent={currentAgent}/>
                 <h3 className='text-white text-xl py-5'>{currentAgent.description}</h3>
                 <div className='border border-white-600 border-2 p-10 [clip-path:_polygon(20%_0,_100%_0,_100%_80%,_80%_100%,0_100%,_0_20%)]'>
                     <h2 className='text-white text-4xl mx-auto font-fat w-fit'>ABILITIES:</h2>
@@ -48,5 +47,24 @@ export function ValoAgent (){
                 </div>
             </div>
         </div>
+        <div className='flex xl:hidden justify-between items-center flex-row gap-2 p-2 py-6'>
+            <img className='w-10 h-10 md:w-24 md:h-24'src={currentAgent.displayIcon}/>
+            <h1 className='text-white text-3xl md:text-6xl font-fat border-b-2 border-red-600'>{currentAgent.displayName.toUpperCase()}</h1>
+            <span className='flex flex-col justify-between items-center border border-red-600 border-2 p-1 md:p-4 [clip-path:_polygon(20%_0,_100%_0,_100%_80%,_80%_100%,0_100%,_0_20%)]'>
+                <h2 className='text-white text-lg md:text-2xl'>Role:</h2>
+                <span className='flex flex-row items-center gap-1 mx-2'>
+                    <img className='h-4 md:h-6'src={currentAgent.role.displayIcon}/>
+                    <h1 className='text-gray-400 text-xl lg:text-2xl'>{currentAgent.role.displayName}</h1>
+                </span>
+            </span>
+            
+        </div>
+        <div className='flex flex-col gap-2 px-4'>
+            <h2 className='text-white text-2xl md:text-4xl mx-auto font-fat w-fit'>ABILITIES:</h2>
+            {currentAgent.abilities.map((ab, index) => 
+                            (<AbilitySM ab={ab}/>)
+                        )}
+        </div>
+        </>
     )
 }
--- stattracker/src/components/imageNavbar/ImageNavbar.jsx
+++ stattracker/src/components/imageNavbar/ImageNavbar.jsx
@@ -1,43 +1,55 @@
 import { NavLink } from "react-router-dom";
 import Slider from 'react-slick'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export function ImageNavbar(object) {
-    const settings = {
-        dots: true,
-        infinite: false,
-        speed: 500,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        initialSlide: 0,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2,
-              initialSlide: 2
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-        ]
-      };
+  var settings = {
+    dots: true,
+    arrows: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 24,
+    slidesToScroll: 0,
+    className: 'z-10 py-2',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 12,
+          slidesToScroll: 12,
+          infinite: false,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 10,
+          slidesToScroll: 10,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 8,
+          slidesToScroll: 8,
+          initialSlide: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 6,
+          arrows: false,
+          slidesToScroll: 5,
+        }
+      }
+    ]
+  };
   return (
-    <nav className="w-screen flex sticky justify-between bg-stone-800 px-2 gap-1 top-0 pt-3">
+    <nav className='w-inherit z-10'>
       <Slider {...settings}>
         {Object.values(object).map((arr) =>
           arr.map((agent, index) => {
@@ -46,7 +58,7 @@ export function ImageNavbar(object) {
                 <div key={index}>
                   <NavLink
                     className={({ isActive }) =>
-                      `relative flex hover:scale-[140%] bg-gradient-to-b to-gray-600 from-red-800 transition z-10 hover:z-50 duration-200 scale ease-in-out [clip-path:_polygon(20%_0,_100%_0,_100%_80%,_80%_100%,0_100%,_0_20%)] ${
+                      `relative flex hover:scale-[140%] bg-gradient-to-b to-gray-600 from-red-800 transition z-20 hover:z-50 duration-200 scale ease-in-out [clip-path:_polygon(20%_0,_100%_0,_100%_80%,_80%_100%,0_100%,_0_20%)] ${
                         isActive
                           ? "bg-gradient-to-b to-gray-600 from-green-800 border border-white-600 border-2"
                           : "hover:bg-stone-800 bg-gradient-to-b to-gray-600 from-green-800 text-white"
@@ -55,7 +67,7 @@ export function ImageNavbar(object) {
                     to={agent.displayName.toLowerCase().replace("/", "")}
                   >
                     <img
-                      className=""
+                      className="z-10"
                       src={agent.displayIcon}
                       alt={agent.displayName}
                     />
