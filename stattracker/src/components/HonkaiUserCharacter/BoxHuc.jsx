21eebe4f41d166bfa80854f4bb23d8821c9a1942
DnlSglsPrz <daniel.segales.perez@hotmail.com>
2024-06-20 05:08:58 +0200

    styled honkusernav

--- stattracker/src/components/HonkaiUserCharacter/BoxHuc.jsx
+++ stattracker/src/components/HonkaiUserCharacter/BoxHuc.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function BoxHUC ({children, stl, title}) {
     return (
       <div className={`rounded-2xl z-50 ${stl}`}>
-        <div className="flex items-center h-5 md:h-10 lg:h-12 rounded-2xl drop-shadow-md group bg-gradient-to-r from-black to-[#42A3AD] border border-gray-300 hover:from-[#42A3AD] hover:to-black">
+        <div className="flex items-center h-5 sm:h-8 md:h-10 lg:h-12 rounded-2xl drop-shadow-md group bg-gradient-to-r from-black to-[#42A3AD] border border-gray-300 hover:from-[#42A3AD] hover:to-black">
           <h2 className="text-left tracking-wide group-hover:tracking-widest transition-all duration-300 ease-in-out items-center p-1.5 pl-2 text-base sm:text-xl md:text-2xl lg:text-3xl text-white font-sans">
             {title}
           </h2>
--- stattracker/src/components/honkai-usernav/HonkaiUserNavChar.jsx
+++ stattracker/src/components/honkai-usernav/HonkaiUserNavChar.jsx
@@ -1,26 +1,31 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-export function HonkaiUserNavChar({obj}) {
+export function HonkaiUserNavChar({ obj }) {
+  const link = "https://cdn.jsdelivr.net/gh/Mar-7th/StarRailRes@master/";
 
-  const link = 'https://cdn.jsdelivr.net/gh/Mar-7th/StarRailRes@master/'
+console.log(obj)
 
-
-    return (
-        <div className="flex justify-between gap-1 w-full lg:w-auto lg:p-1 lg:pr-0 sm:h-[100px] md:h-[115px] transition-all duration-300 ease-in-out"  >
-          {/* <div className="bg-orange-300 rounded-xl h-min border hover:border-2 duration-300 ease-in-out"><img src="/assets/honkai/RuanMeiIcon.png" alt="" className="max-h-24 rounded-xl hover:max-h-28 transition-all duration-300 ease-in-out"/></div>
-          <div className="bg-orange-300 rounded-xl h-min"><img src="/assets/honkai/AcheronIcon.png" alt="" className="max-h-24 rounded-xl hover:max-h-28 transition-all duration-300 ease-in-out"/></div>
-          <div className="bg-violet-500 rounded-xl h-min"><img src="/assets/honkai/Gallagher_Icon.png" alt="" className="max-h-24 rounded-xl hover:max-h-28 transition-all duration-300 ease-in-out"/></div>
-          <div className="bg-orange-300 rounded-xl h-min"><img src="/assets/honkai/Imbibitor_Icon.png" alt="" className="max-h-24 rounded-xl hover:max-h-28 transition-all duration-300 ease-in-out"/></div>
-          <div className="bg-orange-300 rounded-xl h-min"><img src="/assets/honkai/JiaoqiuIcon.png" alt="" className="max-h-24 rounded-xl hover:max-h-28 transition-all duration-300 ease-in-out"/></div>
-          <div className="bg-violet-500 rounded-xl h-min"><img src="/assets/honkai/Luka-Icon.png" alt="" className="max-h-24 rounded-xl hover:max-h-28 transition-all duration-300 ease-in-out"/></div>
-          <div className="bg-orange-300 rounded-xl h-min"><img src="/assets/honkai/Sparkle_Icon.png" alt="" className="max-h-24 rounded-xl hover:max-h-28 transition-all duration-300 ease-in-out"/></div>
-          <div className="bg-orange-300 rounded-xl h-min"><img src="/assets/honkai/Swan_Icon.png" alt="" className="max-h-24 rounded-xl hover:max-h-28 transition-all duration-300 ease-in-out"/></div> */}
-
-          {obj.characters.map((char, index) => (
-            <Link to={`${index}`}>
-              <div className="bg-orange-300 rounded-xl h-min"><img src={link + char.icon} alt="" className="max-h-24 rounded-xl hover:max-h-28 transition-all duration-300 ease-in-out"/></div>
-            </Link>
-          ))}
-        </div>
-    );
-  }
+  return (
+    <div className="flex justify-around gap-1 w-full lg:w-auto lg:p-1 lg:pr-0 sm:h-[100px] md:h-[115px] transition-all duration-300 ease-in-out">
+      {obj.characters.map((char, index) => (
+        <Link to={`${index}`}>
+          <div className=" rounded-xl max-h-24 hover:max-h-28 overflow-hidden drop-shadow-[0_0_-1px_rgba(255,255,255,3)] hover:drop-shadow-[0_0_3px_rgba(255,255,255,0.3)] transition-all duration-300 ease-in-out">
+            <img
+              src={link + char.icon}
+              alt=""
+              className=" rounded-xl max-h-24 hover:max-h-28 hover:scale-[115%] transition-all duration-300 ease-in-out"
+              style={{
+                background:
+                  char.rarity === 5
+                    ? "linear-gradient(#C9A36A 0%, #36211f 75%)"
+                    : char.rarity === 4
+                    ? "linear-gradient(#9864CD 0%, #25243b 75%)"
+                    : "",
+              }}
+            />
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+}
